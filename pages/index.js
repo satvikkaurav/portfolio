203354746b4c4a4361461cdcd5a0e451fe19e3f4
Satvik Kaurav <satvikkaurav10@gmail.com>
2021-08-30 16:26:33 +0530

    Responsive hello

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import styles from '../styles/Home.module.scss';
 import Nav from "../components/Nav";
 import Footer from '../components/Footer'
 import Background from '../components/Background'
+import Buttons from '../components/Buttons';
 
 import Head from 'next/head'
 
@@ -12,16 +13,20 @@ export default function Home() {
         <title>Satvik Kaurav</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
-    <div className={styles.main}>
     <Nav />
 
+    <div className={styles.main}>
+
+    <Background />
+
       <div className = {styles.main__intro}>
+        
         <span className={styles.hello}>Hi ! I am Satvik Kaurav</span>
         <span className={styles.web}>Website Developer || Linux Enthusiast</span>
-      </div>
-
+        {/* <Buttons buttonSize="btn__medium" buttonStyle="btn__outline">Connect to me</Buttons> */}
+        </div>
     </div>
-    <Background />
+    
     
     
     
